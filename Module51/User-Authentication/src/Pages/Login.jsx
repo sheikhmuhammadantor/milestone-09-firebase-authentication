a69dce12a686258090d29e92709c9da3bf9cc4be
Sheikh Muhammad Antor <sheikhmuhammadantor@gmail.com>
2024-11-12 08:19:07 +0600

    Module 51 : Work On Log in Page - SignIn with email and password.

--- Module51/User-Authentication/src/Pages/Login.jsx
+++ Module51/User-Authentication/src/Pages/Login.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import { AuthContext } from '../Providers/AuthProvider';
 
 function Login() {
+
+  const { signInUser } = useContext(AuthContext)
 
   const handelLogIn = (e) => {
     e.preventDefault();
     const email = e.target.email.value;
     const password = e.target.password.value;
-    console.log(email, password);
+
+    signInUser(email, password)
+      .then(result => console.log(result.user))
+      .catch(error => console.log("ErroR : ", error))
   }
 
   return (
--- Module51/User-Authentication/src/Providers/AuthProvider.jsx
+++ Module51/User-Authentication/src/Providers/AuthProvider.jsx
@@ -1,4 +1,4 @@
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 import React, { createContext } from 'react'
 import { auth } from '../Firebase/firebase';
 
@@ -6,15 +6,17 @@ export const AuthContext = createContext(null);
 
 function AuthProvider({ children }) {
 
-    const name = "Sheikh Muhammad";
-
     const createUser = (email, password) => {
         return createUserWithEmailAndPassword(auth, email, password);
     }
 
+    const signInUser = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
     const authInfo = {
-        name,
         createUser,
+        signInUser,
     }
 
     return (
